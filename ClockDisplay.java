b22f86020f0f72f322549997bd6c57db0df4f85e
Tom Wentz <twentz15@outlook.com>
2016-02-17 10:36:27 -0500

    Changed the clock type
    Changed the numbers from 24 -> 13 and added an time tick for the hours
    so it would display 1 not 0

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -24,7 +24,7 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
@@ -36,7 +36,7 @@ public class ClockDisplay
      */
     public ClockDisplay(int hour, int minute)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
@@ -49,6 +49,9 @@ public class ClockDisplay
     {
         minutes.increment();
         if(minutes.getValue() == 0) {  // it just rolled over!
+            hours.increment();
+        }
+        if(hours.getValue() == 0) {  // it just rolled over!
             hours.increment();
         }
         updateDisplay();
